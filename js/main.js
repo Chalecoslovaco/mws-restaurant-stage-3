59f8b2c01113c43bc9355e68551a45698b0935a7
Sergio <sergiogarher@gmail.com>
2018-06-28 18:24:36 +0200

    feat: add toggle_map functionality for better performance (first meaningful paint)

--- js/main.js
+++ js/main.js
@@ -71,6 +71,7 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  * Initialize Google map, called from HTML.
  */
 window.initMap = () => {
+  toggle_map();
   let loc = {
     lat: 40.722216,
     lng: -73.987501
